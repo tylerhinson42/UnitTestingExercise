55270c2e93e54201dd904c8ab3270df0785b8a34
Tyler Hinson <tyler.hinson42@gmail.com>
2020-04-17 18:12:49 -0500

    Made some changes

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,36 +1,58 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace UnitTestingExercise.Tests
 {
     public class UnitTests
     {
+        private IEnumerable<object> sum;
+
         [Theory]
-        [InlineData()]
+        [InlineData(0, 0, 0, 0)]
+#pragma warning disable xUnit1024 // Test methods cannot have overloads
         public void Add(int num1, int num2, int num3, int expected)
+#pragma warning restore xUnit1024 // Test methods cannot have overloads
         {
             //Arrange
-
+            var newInt = new UnitTests();
             //Act
-
+            object v = Add(num1 + num2 + num3);
+            object actual = v;
             //Assert
+            Assert.Equal(sum, actual);
+        }
 
+#pragma warning disable xUnit1024 // Test methods cannot have overloads
+        private static object Add(int v)
+#pragma warning restore xUnit1024 // Test methods cannot have overloads
+        {
+            throw new NotImplementedException();
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(0, 0, 0)]
+#pragma warning disable xUnit1024 // Test methods cannot have overloads
         public void Subtract(int minuend, int subtrhend, int expected)
+#pragma warning restore xUnit1024 // Test methods cannot have overloads
         {
             //Arrange
-
+            var newInt = new UnitTests();
             //Act
-
+            object actual = UnitTests.Subtract(minuend - subtrhend - expected);
             //Assert
+            Assert.Equal(sum, actual);
+        }
 
+#pragma warning disable xUnit1024 // Test methods cannot have overloads
+        private static object Subtract(int v)
+#pragma warning restore xUnit1024 // Test methods cannot have overloads
+        {
+            throw new NotImplementedException();
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(0, 0, 0)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
@@ -42,7 +64,7 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(0, 0, 0)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
